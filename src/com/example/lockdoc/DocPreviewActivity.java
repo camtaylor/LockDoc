1007feb762e362670dde065615dfe5fbd9d6b8d8
Cameron Taylor <camerontaylor00@gmail.com>
2014-11-09 16:43:02 -0800

    finished edit from list

--- src/com/example/lockdoc/DocPreviewActivity.java
+++ src/com/example/lockdoc/DocPreviewActivity.java
@@ -1,5 +1,6 @@
 package com.example.lockdoc;
 
+// Delete me later
 import java.io.File;
 
 import android.app.Activity;
@@ -28,13 +29,21 @@ public class DocPreviewActivity extends ActionBarActivity {
 	private static int TAKE_PICTURE = 1;
 	private Uri imageUri;
 	private static String logtag = "DocPreviewActivity";
+	boolean newDoc = true;
+	long id;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_doc_preview);
-		takePhoto();
-
+		Bundle extras = getIntent().getExtras();
+		if (extras == null)
+			takePhoto();
+		else {
+			id = extras.getLong("ID");
+			editPreview(id);
+			newDoc = false;
+		}
 	}
 
 	private void takePhoto() {
@@ -47,6 +56,24 @@ public class DocPreviewActivity extends ActionBarActivity {
 		imageUri = Uri.fromFile(photo);
 		intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
 		startActivityForResult(intent, TAKE_PICTURE);
+	}
+
+	private void editPreview(long id) {
+		DocSave db = new DocSave(this);
+		Document doc;
+
+		db.open();
+		doc = db.getDocumentAtCursor(id);
+
+		db.close();
+		// Query database for obj with ID
+		setContentView(R.layout.activity_doc_preview);
+		EditText name = (EditText) findViewById(R.id.doc_name);
+		name.setText(doc.getFilename());
+		EditText type = (EditText) findViewById(R.id.doc_type);
+		type.setText(doc.getDocType());
+		// EditText description = (EditText) findViewById(R.id.doc_description);
+		// type.setText(doc.getDocDescription());
 	}
 
 	@Override
@@ -84,8 +111,9 @@ public class DocPreviewActivity extends ActionBarActivity {
 		// TODO delete
 		Document doc = new Document(name, type);
 		String date = doc.getUploadDate();
-		// TODO save in db and start new activity for classification
+		// save in db and start new activity for classification
 		boolean didItWork = true;
+		if(newDoc){
 		try {
 			DocSave entry = new DocSave(this);
 			entry.open();
@@ -98,8 +126,15 @@ public class DocPreviewActivity extends ActionBarActivity {
 				Toast.makeText(getApplicationContext(), "Image saved",
 						Toast.LENGTH_LONG).show();
 			}
-		}	
-		//starts file list after saving to database
+		}
+		}
+		else{
+			DocSave entry = new DocSave(this);
+			entry.open();
+			entry.editEntry(id, name, type);
+			entry.close();
+		}
+		// starts file list after saving to database
 		Intent view = new Intent(this, FileListActivity.class);
 		startActivity(view);
 	}
